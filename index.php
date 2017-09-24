6e659b9dd4efbed14d34cd3e00503b2f915dcefb
jaikun <jaicelim@gmail.com>
2017-09-24 17:36:20 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Quicksand|Roboto" rel="stylesheet"> 
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="css/index.css">
+	</head>
+	<body>
+		<video playsinline autoplay muted loop poster="images/indexbg.jpg" id="bgvid">
+    		<source src="videos/index-mov.mp4" type="video/mp4">
+		</video>
+
+		<nav>
+			<img class="logo-tagline" src="images/trex-worldmark.png">
+		</nav>
+		<div id="side-nav">
+			<a href="home.html">Home</a>
+			<a href="movies.html">Movies</a>
+			<a href="talents.html">Talents</a>
+			<a href="aboutus.html">About Us</a>
+		</div>
+		<center>
+			<div id="intro" class="container">
+				
+			</div>
+		</center>
+
+		<script src="bootstrap.min.js"></script>
+		<script src="js/jquery-2.2.0.min.js"></script>
+	</body>
+</html>
